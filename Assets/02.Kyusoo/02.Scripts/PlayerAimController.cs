d9073cf1b843484bcbc410dd01b57ccdc23f1dd2
kimkyusoo <rbtn110@gmail.com>
2026-06-08 16:26:16 +0900

    작업 결과물에 따른 프리팹 미전환한 부분들 일괄 추가 - 크로스헤어, 플레이어 정면 프리뷰

--- Assets/02.Kyusoo/02.Scripts/PlayerAimController.cs
+++ Assets/02.Kyusoo/02.Scripts/PlayerAimController.cs
@@ -8,8 +8,13 @@ public class PlayerAimController : MonoBehaviour, IAimProvider
     [SerializeField] private float _maxAimDistance = 100f;
     [SerializeField] private LayerMask _aimLayerMask;
 
+    [Header("근접 조준 보정관련 설정")]
+    [SerializeField] private float _closeFadeDistance = 5f;
+    [SerializeField] private float _virtualDistance = 25f;
+
     [Header("UI 연동")]
     [SerializeField] private Image _crosshairImage;
+
 
     public Vector3 CurrentAimPoint { get; private set; }
 
@@ -28,14 +33,29 @@ public class PlayerAimController : MonoBehaviour, IAimProvider
         Vector3 screenCenter = new Vector3(Screen.width / 2f, Screen.height / 2f, 0f);
         Ray ray = _mainCamera.ScreenPointToRay(screenCenter);
 
+        float finalRayDistance = _maxAimDistance;
+
         if (Physics.Raycast(ray, out RaycastHit hit, _maxAimDistance, ~_aimLayerMask))
         {
-            CurrentAimPoint = hit.point;
+            float distanceToPlayer = Vector3.Distance(transform.position, hit.point);
+
+            if (distanceToPlayer < _closeFadeDistance)
+            {
+                float distanceRatio = Mathf.Clamp01(distanceToPlayer / _closeFadeDistance);
+
+                finalRayDistance = Mathf.Lerp(_virtualDistance, hit.distance, distanceRatio);
+            }
+            else
+            {
+                finalRayDistance = hit.distance;
+            }
         }
         else
         {
-            CurrentAimPoint = ray.origin + ray.direction * 25f;
+            finalRayDistance = 20f;
         }
+
+        CurrentAimPoint = ray.origin + ray.direction * finalRayDistance;
     }
 
     public Vector3 GetAimPoint()
