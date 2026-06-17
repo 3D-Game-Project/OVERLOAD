58906344757d2985e623176be3ddd18f2e350e17
9Kyun <helioty131@naver.com>
2026-06-17 10:06:29 +0900

    enemy major bug fix

--- Assets/02.Kyusoo/06.Behavior/EnemyPattern/PatrolNavigateAction.cs
+++ Assets/02.Kyusoo/06.Behavior/EnemyPattern/PatrolNavigateAction.cs
@@ -47,7 +47,12 @@ public partial class PatrolNavigateAction : Action
         
         if (_agent == null || PatrolPositions?.Value == null || PatrolPositions.Value.Count == 0)
             return Status.Failure;
- 
+
+        if (!_agent.gameObject.activeInHierarchy || !_agent.enabled || !_agent.isOnNavMesh)
+        {
+            return Status.Running;
+        }
+
         if (CurrentPatrolIndex.Value < 0 || CurrentPatrolIndex.Value >= PatrolPositions.Value.Count)
         {
             CurrentPatrolIndex.Value = 0;
