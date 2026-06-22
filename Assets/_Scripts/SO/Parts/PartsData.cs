e05f00325c3d0e65515e10095ebd71d83d0754c7
9Kyun <helioty131@naver.com>
2026-06-23 01:58:23 +0900

    Update Ui Image

--- Assets/_Scripts/SO/Parts/PartsData.cs
+++ Assets/_Scripts/SO/Parts/PartsData.cs
@@ -9,6 +9,7 @@ public abstract class PartsData : ScriptableObject
     [SerializeField] private float _maxDurability = 300;
     [SerializeField] private int _price;
     [TextArea(1,3)][SerializeField] private string _description;
+    [SerializeField] private CurrencyCost _buyCost;
 
     public string PartsName => _partsName;
     public GameObject PartsPrefab => _partsPrefab;
@@ -23,4 +24,6 @@ public abstract class PartsData : ScriptableObject
     public string Description => _description;
 
     public abstract PartsType PartsType { get; }
+
+    public CurrencyCost BuyCost => _buyCost;
 }
